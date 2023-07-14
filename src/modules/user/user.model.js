58442e54070042114ce64438ebfa4a8f00392bba
rivalcoba <ivan.rivalcoba@gmail.com>
2023-07-14 17:08:09 +0000

    ⭐️ Add user model

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -35,6 +35,19 @@ const UserSchema = new Schema({
       trim: true,
       unique: true,
     },
+    password: {
+      type: String,
+      required: [true, 'Password is required!'],
+      trim: true,
+      minlength: [6, 'Password need to be longer'],
+      validate: {
+        validator(password) {
+          // TODO: return passwordReg.test(password);
+          return password !== '';
+        },
+        message: '{VALUE} is not a valid password!',
+      },
+    },
   },
 });
 
