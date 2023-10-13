e8705761069977883d36b527aa5622d50cb9f9b0
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-10-13 13:46:38 +0000

    🌟 ENHANCE: route.js Add next arg to General Error Handler

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,8 @@ export default function addRoutes(app) {
   });
 
   // General Error Handler
-  app.use((err, req, res) => {
+  // eslint-disable-next-line
+  app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
