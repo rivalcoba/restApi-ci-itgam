21ae9293fdda9795b2dffa9001bde4c6e09fe07a
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-10-16 05:49:40 +0000

    Corrección De Error Debug

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,46 +3,26 @@
 /**
  * Module dependencies.
  */
-
-import app from '../app';
-import debug from 'debug';
+import Debug from 'debug';
 import http from 'http';
+import app from '../app';
 
-
-const debug = debug('restapi-ci-itgam:server');
-
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-
-
+const debug = Debug('restapi-ci-itgam:server');
 
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app); 
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port); 
-server.on('error', onError);
-server.on('listening', onListening);
+const server = http.createServer(app);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) { 
+function normalizePort(val) {
   const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
+  if (Number.isNaN(port)) {
     // named pipe
     return val;
   }
@@ -56,6 +36,13 @@ function normalizePort(val) {
 }
 
 /**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
  * Event listener for HTTP server "error" event.
  */
 
@@ -64,9 +51,7 @@ function onError(error) {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? `Pipe ${port}`
-    : `Port ${port}`;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -89,8 +74,14 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
   debug(`Listening on ${bind}`);
 }
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
