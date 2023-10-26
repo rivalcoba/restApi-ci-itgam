2381ac8964754da44e85bb0799880e5c8160e13a
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-10-26 19:44:14 +0000

    🌟 Feat: routes Establece la estructura para routes.js con modules

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,21 @@
-import createError from 'http-errors';
-
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+// import usersRouter from './routes/users';
+import homeRouter from './modules/home/home.route';
+import { error404, generalError } from './modules/error/error.controller';
 
 export default (app) => {
+  /*
   app.use('/', indexRouter);
   app.use('/users', usersRouter);
+  */
+  // HOME
+  // GET: /
+  app.use('/', homeRouter);
 
   // catch 404 and forward to error handler
   app.use((req, res, next) => {
-    next(createError(404));
+    next(error404);
   });
 
   // error handler
-  app.use((err, req, res) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-    console.log(err);
-  });
+  app.use(generalError);
 };
