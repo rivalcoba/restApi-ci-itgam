09dae4a73df40ddff833861b78b58aace5e1f433
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-10-26 16:19:06 +0000

    🐞 SOLVE: Enrutado Mueve los manejadores de error de middlewares al archivo routes

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,4 +1,3 @@
-import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
@@ -29,20 +28,4 @@ export default (app) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static('public'));
-
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    next(createError(404));
-  });
-
-  // error handler
-  app.use((err, req, res) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-    console.log(err);
-  });
 };
